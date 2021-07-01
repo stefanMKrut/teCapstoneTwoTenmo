8736fcc47c45dbefeb16e71978d62f56385cde34
Joshua Olsen <joshua.olsen14@gmail.com>
2021-07-01 10:30:22 -0400

    changes to updateBalance method.

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -10,6 +10,6 @@ public interface AccountDao {
 
     BigDecimal getBalance(String username);
 
-    boolean updateBalance(BigDecimal amount, String username);
+    boolean updateBalance(BigDecimal amount, int userId);
 
 }
